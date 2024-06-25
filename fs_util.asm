5615e7f1379c9e3b768a72e795de165de06e4175
Alexandr Shunco <80820965+anythinggwp@users.noreply.github.com>
2024-06-25 15:52:27 +0300

    Update fs_util.asm

--- fs_util.asm
+++ fs_util.asm
@@ -1,263 +1,219 @@
 .386
 dat segment use16
-adr_packlog dd 0
-  dw 1, buff, dat
-adr_FAT dd 0
-adr_RD dd 0
-adr_FCLAST dd 0
-сluster_size db 0
-disk_letter db 2, 3 dup(?), 0Ah, 0Dh, '$'
-DIR_buff db 8, 11 dup(20h), 0Ah, 0Dh, '$'
-buff db 512 dup(?)
-buff_FAT db 512 dup(?)
-msg_enter_disk db 'Pleas enter the logical disk letter:', 0Ah, 0Dh, '$'
-msg_enter_DIR db 'Pleas enter the short DIR name:', 0Ah, 0Dh, '$'
+adr_pack db 16, 0, 1, 0
+   dw buff, dat
+   dq 0
+buff db 512 dup(0)
+msg_disk db 'physical disk 0:', , 0Ah, 0Dh, '$'
+msg db 'logdisk - ', 11 dup(0), 'filesystem - ', 8 dup(0), 'capacity: lba = ', 10 dup(0), 'mbyte = ', 7 dup(0), , 0Ah, 0Dh, '$'
+not_found db 'Extended partition or disk doesnt exist', 0Ah, 0Dh, '$'
+first_EPR dd 0
+prev_EPR dd 0
+disk_ntfs db 'NTFS'
 dat ends
 lab segment use16
 Assume ds:dat, cs:lab
-   init_memseg: mov bx, dat
-        mov ds, bx
-        mov bx, dat
-        mov es, bx
-;##########################################
-      init_path: lea dx, msg_enter_disk 
+  init_mem_seg:  mov bx, dat
+      mov ds, bx
+      mov bx, ds
+      mov es, bx
+;###############################################
+  init_disk_0: call show_disk
+      mov ah, 42h
+      lea si, adr_pack
+      mov dl, 80h
+      int 13h
+      jnc short search_init
+      lea dx, not_found
+      call def_msg
+  init_disk_1: call show_disk
+      mov ah, 42h
+      lea si, adr_pack
+      mov dl, 81h
+      int 13h
+      jnc short search_init
+      lea dx, not_found
+      call def_msg
+  init_disk_2: call show_disk
+      mov ah, 42h
+      lea si, adr_pack
+      mov dl, 82h
+      int 13h
+      jnc short search_init
+      lea dx, not_found
+      call def_msg
+  init_disk_3: call show_disk
+      mov ah, 42h
+      lea si, adr_pack
+      mov dl, 83h
+      int 13h
+      jnc short search_init
+      lea dx, not_found
+      call def_msg
+   fin:  mov ah, 4Ch
+           int 21h
+;#####################################################
+        search_init: lea si, buff
+               mov cx, 4
+;#####################################################
+         srch_MBR:  cmp byte ptr ds:[si+450], 5h
+            je short init_first_EPR
+          cmp byte ptr ds:[si+450], 0Fh
+          je short init_first_EPR
+            add si, 10h
+          loop srch_MBR
+          lea dx, not_found
+          call def_msg
+;#####################################################
+        srch_disk: cmp msg_disk+14, 31h
+          je short init_disk_1
+          cmp msg_disk+14, 32h
+          je short init_disk_2
+          cmp msg_disk+14, 33h
+          je short init_disk_3
+          jmp short fin
+;#####################################################
+     init_first_EPR: add si, 454
+            lodsd
+            mov dword ptr first_EPR, eax
+            lea di, adr_pack+8
+            stosd
+            call switch
+;#####################################################
+     init_serch_EPR:   lea si, buff
+;#####################################################
+       search_EPR: cmp byte ptr ds:[si+450], 0
+             je fin_EPR
+           cmp byte ptr ds:[si+450], 5h
+          je last_crypt
+          cmp byte ptr ds:[si+450], 0Fh
+          je last_crypt
+          mov eax, dword ptr ds:[si+458]
+          lea di, msg+67
+          call translate ; Добавляем в сообщение LBA координаты 
+          mov eax, dword ptr ds:[si+458]
+          mov ebx, 512
+          mul ebx
+          mov ebx, 1024
+          div ebx
+          div ebx
+          lea di, msg+82
+          call translate
+          cmp byte ptr ds:[si+450], 07h
+          jne short log_system_fat32
+;####################################################
+ log_system_ntfs: lea di, msg+16
+      lea si, disk_ntfs
+      movsd
+      lea di, msg+37
+      movsd
+      lea si, buff
+      lea dx, msg
          call def_msg
-         lea dx, disk_letter ; ввод буквы
-         mov ah, 0Ah
-         int 21h
-         lea dx, msg_enter_DIR 
+      jmp short next_crypt
+;####################################################
+   log_system_fat32: cmp byte ptr ds:[si+450], 0Bh
+         jne short log_system_fat16
+         lea si, adr_pack+8
+         lea di, prev_EPR
+         movsd
+         lea si, buff
+         call init_srch_FAT
+         call switch ; Попадаем в bootsector
+         lea si, buff
+         add si, 71
+         call add_logname ; Добавляем имя и название файловой системы
+         lea si, prev_EPR
+         lea di, adr_pack+8
+         movsd
+         call switch
+         lea dx, msg
          call def_msg
-         lea dx, DIR_buff ; ввод имени каталога
-         mov ah, 0Ah
-         int 21h
-;##########################################
-        init_log_disk: call switch
-     jc short fin
-;##########################################
-         init_FAT_adr: mov eax, 0
-      mov ax, word ptr buff+14
-       mov dword ptr adr_FAT, eax
- init_RD_adr:    add ax, word ptr buff+22
-    add ax, word ptr buff+22
-    mov dword ptr adr_RD, eax
-       mov dword ptr adr_packlog, eax
- init_FCLAST:  mov ax, word ptr buff+17
-    mov dx, 32
-    mul dx
-    mov bx, 512
-    div bx
-    mov word ptr adr_FCLAST, ax
-    lea si, adr_RD
-    lodsd
-    add dword ptr adr_FCLAST, eax
- init_CLSIZE:  mov al, buff+0Dh
-    mov byte ptr сluster_size, al    
-;##########################################
- init_rootDIR:  lea si, adr_RD
-    lea di, adr_packlog
-    movsd
-    call switch
-       jc short fin
-;##########################################  
-    next_page:  mov cx, 19
- init_ownDIR:  lea di, buff
- next_record:  lea si, DIR_buff+2
-    cmpsd
-    jne short not_match_0
-    inc si ; Пофиксить
- next_half:  cmpsd
-      je short open_DIR ; Каталог найден
-      jne short not_match_1
-not_match_0:  add di, 4h  ; смещаемся к следующей записи
- not_match_1:  add di, 18h ; смещаемся к следующей записи
-      dec cx
-      jne short next_record
-      add dword ptr adr_packlog, 1
-      call switch
-      lea si, adr_packlog
-      lea di, adr_FCLAST
-      cmpsd
-      jne short next_page
-;###########################################
-  fin:  mov ah, 4ch
-         int 21h
-;###########################################
- open_DIR:  mov ax, word ptr ds:[di+12h] ; номер кластера каталога
-    sub ax, 2
-    lea si, adr_FCLAST
-    lea di, adr_packlog
-    movsd
-; Считаем сектора до нашего каталога
-    mov bx, 0
-    mov bl, byte ptr сluster_size
-    mul bx
-; Складываем с адресом первого кластера
-    add word ptr adr_packlog, ax
-    add word ptr adr_packlog, dx
-    mov eax, dword ptr adr_packlog
-    push eax 
-;############################################
-    call switch
-    lea si, adr_FAT
-    lea ax, buff_FAT
-    call switch_buff
-    call switch    
-; Считаем размер каталога #############################
-   init_clust_calc: mov cx, 0
-      mov bl, сluster_size
-      mov bh, 0
-      push bx 
-      push cx ; сохраняем в стек счетчик кол-ва кластеров
-      mov eax, dword ptr adr_FAT
-      push eax
-      mov bx, word ptr buff+1Ah
-      lea si, buff_FAT
-      shl bx, 1 
-      mov bp, bx
-      ;add bp, si
-      cmp bp, 200h
-      jb short clust_count 
-     je short clust_count
-     add dword ptr adr_packlog, 1
-     call switch
-       clust_count: cmp word ptr ds:[bx+si+0h], 0FFFFh
-          je short init_check_rec
-; Каталог не закончился ###############################
-          pop eax
-          pop cx
-          inc cx
-          push cx
-          push eax
-          inc si
-          inc si
-          jmp short clust_count
-; Ищем файлы в кластере ###############################
-    init_check_rec:  pop eax
-       mov dword ptr adr_FAT, eax 
-       mov dword ptr adr_packlog, eax 
-       call switch
-       mov bx, 0
-       push bx ; смещение для FAT
-       lea si, buff+40h
-  chek_del:  cmp byte ptr ds:[si], 0E5h
-     je short chek_file_0 
-     add si, 20h
-     jmp short last_adr              chek_file_0:  cmp byte ptr ds:[si+0bh], 0fh
-    jne short chek_file_1
-    add si, 20h
-    jmp short last_adr
-; Проверяем на подкаталлог 
-       chek_file_1: test byte ptr ds:[si+0bh], 00010000b
-    jz short chek_file_2_0
-    add si, 20h
-    jmp short last_adr
-; Проверяем что файл удален мягко        
- chek_file_2_0:   lea di, buff_FAT
-      mov bx, word ptr ds:[si+1Ah] 
-     cmp bx, 0
-     jne short chek_file_2_2
-     add si, 20h
-     jmp short last_adr
-chek_file_2_2: shl bx, 1 
-     pop ax 
-     sub bx, ax
-     push ax 
-     cmp bx, 200h
-     jb short chek_file_2_1
-     je short chek_file_2_1 
-     pop ax 
-     add ax, bx 
-     push ax 
-     add dword ptr adr_FAT, 1
-     lea si, adr_FAT
-    lea ax, buff_FAT
-    call switch_buff
-    call switch
-    jmp short chek_file_2_0 ; возвращаемся к проверке файла
- chek_file_2_1: cmp word ptr ds:[bx+di+0h], 0h
-     je short last_adr
-    mov byte ptr ds:[si], 40h
- last_adr:  lea ax, buff+1E0h
-    cmp si, ax 
-    jna short chek_del
-    pop bp 
-    pop cx 
-    pop bx 
-    dec bx 
-    jz short last_clust
-; записываем на диск сектор из буфера
-    pop eax 
-    mov dword ptr adr_packlog, eax
-    lea dx, buff 
-    mov word ptr adr_packlog+6, dx 
-    push eax
-    push bx
-    push cx
-    push bp
-    call write
-    pop bp
-    pop cx
-    pop bx
-; Смещаемся в следующий сектор
-    pop eax 
-    inc eax 
-    mov dword ptr adr_packlog, eax 
-    push eax ; сохраняем в стек новый адрес
-    lea ax, buff
-    mov word ptr adr_packlog+6, ax 
-    push bx ; отправляем оставшееся кол-во обратно в стек
-    push cx 
-    push bp 
-    call switch ; загружаем следующий сектор
-    lea si, buff
-    jmp chek_del
-last_clust:   cmp cx, 0
-    je fin ; если каталог закончился
-    dec cx
-    pop bx 
-    mov bl, byte ptr сluster_size
-    pop eax
-    inc eax
-    mov dword ptr adr_packlog, eax
-    push eax
-    push bx ; сохраняем в стек кол-во секторов в кластере
-    push bp 
-    lea ax, buff
-    mov word ptr adr_packlog+6, ax
-    call switch
-    lea si, buff
-     jmp chek_del ; если каталог не закончился
+         jmp short next_crypt
+;####################################################
+   log_system_fat16: cmp byte ptr ds:[si+450], 06h
+         jne short last_crypt
+         lea si, adr_pack+8
+         lea di, prev_EPR
+         movsd
+         lea si, buff
+         call init_srch_FAT
+         call switch ; Попадаем в bootsector
+         lea si, buff
+         add si, 43
+         call add_logname ; Добавляем имя
+         lea si, prev_EPR
+         lea di, adr_pack+8
+         movsd
+         call switch
+         lea dx, msg
+         call def_msg
+;####################################################
+   next_crypt: lea si, buff+16
+       jmp search_EPR
+   last_crypt: add si, 454
+      lodsd
+      add eax, dword ptr first_EPR
+      lea di, adr_pack+8
+      stosd
+         call switch
+         jmp init_serch_EPR
+      fin_EPR: jmp srch_disk
 
-   switch proc
-       mov al, disk_letter+2
-     sub al, 41h
-     mov cx, 0FFFFh
-     mov dx, 0h
-     lea bx, dword ptr adr_packlog
-     int 25h
-     pop ax
-       RET
-   switch endp
-   switch_buff proc
-    lea di, adr_packlog
-    movsd
-    mov word ptr adr_packlog+6, ax
-       RET
-   switch_buff endp
-   write proc
-       mov al, disk_letter+2
-       sub al, 41h
-       mov cx, 0FFFFh
-       mov dx, 0h
-       lea bx, dword ptr adr_packlog
-       int 26h
-       pop ax
-       RET
-   write endp
-   def_msg proc
-    mov ah, 9h
-    int 21h
+
+
+ translate proc 
+   start:  mov ebx, 10
+          mov edx, 0
+          div ebx
+          add dl, 30h
+          mov byte ptr ds:[di], dl
+          dec di
+          cmp ax, 0Ah
+          ja short start
+          je short start
+          add al, 30h
+          mov byte ptr ds:[di], al
+      RET
+ translate endp
+ switch proc
+   mov ah, 42h    
+   lea si, adr_pack  
+   mov dl, byte ptr msg_disk+14 ; Перемещаем в регистр сиволический номер ЖД 
+   add dl, 4Fh ; Переформатируем его для прервания
+   int 13h
+   RET 
+ switch endp
+ show_disk proc
+      mov ah, 9h
+      lea dx, msg_disk
+      int 21h
+      add msg_disk+14, 1h
+      RET
+ show_disk endp
+ def_msg proc
+  mov ah, 9h
+  int 21h
+  RET
+ def_msg endp
+ init_srch_FAT proc
+  add si, 454
+   lodsd
+   add dword ptr adr_pack+8, eax
+   RET
+ init_srch_FAT endp
+ add_logname proc
+    mov cx, 11 
+    lea di, msg+10
+ beg_logn: movsb
+       loop beg_logn
+       mov cx, 8
+       lea di, msg+33
+   beg_logFS: movsb
+       loop beg_logFS
     RET
- def_msg endp         
+ add_logname endp
+            
+
 lab ends
- End init_memseg
+ End init_mem_seg
